3d63619c36269c940716bf59004e836cb792a05e
rjustincardona <rjustincardona@gmail.com>
2024-07-31 12:33:15 -0400

    augmenting AAA structure

--- duality/dual_check.jl
+++ duality/dual_check.jl
@@ -13,15 +13,16 @@ mutable struct Surrogate
         a1 = rand(Float64)
         s = rand(ComplexF64, n)
         A = rand(ComplexF64, n, n);
-        P0 = rand(ComplexF64, n, n);
-        P1 = (A + Adjoint(A)) / 2;
+        P0 = (A + Adjoint(A)) / 2;
+        P1 = rand(ComplexF64, n, n);
         new(a0, a1, s, P0, P1)
     end
 end
 
 
 function eval(surr::Surrogate, xi::Float64)
-    M = (surr.a1 + xi) .* surr.P0 + surr.a0 .* surr.P1;
+    # M = (surr.a1 + xi) .* surr.P0 + surr.a0 .* surr.P1;
+    M = (surr.a1 + xi) .* surr.P1;# + surr.a0 .* surr.P1;
     x = M \ surr.s;
     return ((Adjoint(x) * surr.s).im - Adjoint(x) * surr.P0 * x).re    
 end
@@ -46,13 +47,15 @@ function dual_check(s, fn_acc, err_acc, xi_guess::Float64, width::Int64, depth::
     _, _, z = prz(a)
     try
         z = maximum(filter(x -> abs(a(x)) < 1e-6, map(x -> x.re, z)))
-    return dual_check(s, push!(fn_acc, a), push!(err_acc, abs(eval(s, z))), z, width, depth - 1, depth_true, samples_domain)
+        return dual_check(s, push!(fn_acc, a), push!(err_acc, abs(eval(s, z))), z, width, depth - 1, depth_true, samples_domain)
     catch
         domain = range(xi_guess - 0.1 * abs(xi_guess), xi_guess + 0.1 * abs(xi_guess), 100)
         plot(domain, map(x -> eval(s, x), domain))
         savefig("preview.png")
         println(z)
+        println(depth)
         readline()
+        return dual_check(s, fn_acc, err_acc, 2 * xi_guess, width, depth, depth_true, samples_domain)
     end
 end
 
@@ -80,16 +83,6 @@ function err_analysis(xi_init::Float64, hyperparameters, depth::Int64, reps::Int
     return map(h -> error_statistics(nothing, xi_init, h[1], h[2], depth, reps), hyperparameters)
 end
 
-
-# xi_init = 1
-# samples = 100
-# s_init = Surrogate(10)
-# domain = range(xi_init - 0.2 * abs(xi_init), xi_init + 0.2 * abs(xi_init), samples)
-# a_s = aaa(domain,  xi -> eval(s_init, xi), mmax = Int64(floor(samples / 2)))
-# errs = map(xi -> (abs(a_s(xi) - eval(s_init, xi))), domain)
-# err_mean = foldl(+, errs) / samples
-# _, _, err_new = dual_check(s_init, [a_s], [err_mean], 1.0, 10, 5, 5, domain)
-# println(err_new)
 
 samples_domain = 10:20
 width_domain = 4:10
