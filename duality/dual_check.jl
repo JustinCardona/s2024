7c19f1a5556060ba10123b6a7b3719cb7a4a1810
rjustincardona <rjustincardona@gmail.com>
2024-08-05 15:00:22 -0400

    AAA new testing

--- duality/dual_check.jl
+++ duality/dual_check.jl
@@ -1,4 +1,7 @@
-using LinearAlgebra, BaryRational, Serialization, Plots
+using LinearAlgebra, BaryRational, Serialization, Plots, Distributed
+import Base.:+
+
+a::BaryRational.AAAapprox + b::BaryRational.AAAapprox = BaryRational.AAAapprox(vcat(a.x, b.x), vcat(a.f, b.f), vcat(a.w, b.w), vcat(a.errvec, b.errvec))
 
 
 mutable struct Surrogate
@@ -21,9 +24,8 @@ end
 
 
 function eval(surr::Surrogate, xi::Float64)
-    # M = (surr.a1 + xi) .* surr.P0 + surr.a0 .* surr.P1;
-    M = (surr.a1 + xi) .* surr.P1;# + surr.a0 .* surr.P1;
-    x = M \ surr.s;
+    M = (surr.a1 + xi) .* surr.P1
+    x = M \ surr.s
     return ((Adjoint(x) * surr.s).im - Adjoint(x) * surr.P0 * x).re    
 end
 
@@ -36,58 +38,42 @@ function update(surr::Surrogate, dx::Float64)
 end
 
 
-function dual_check(s, fn_acc, err_acc, xi_guess::Float64, width::Int64, depth::Int64, depth_true::Int64, samples_domain)
+function dual_check(s::Surrogate, approx::BaryRational.AAAapprox, err_acc, xi_guess::Float64, width::Int64, depth::Int64, tol = 1e-3)
     if depth == 1
-        return xi_guess, fn_acc, err_acc[1:depth_true]
+        return approx, err_acc, xi_guess
     end
-    approx = xi -> foldl(+, map(f -> f(xi), fn_acc))
-    update(s, 1e-5)
-    domain = range(xi_guess - 0.1 * abs(xi_guess), xi_guess + 0.1 * abs(xi_guess), width)
-    a = aaa(domain, xi -> eval(s, xi) - approx(xi))
-    _, _, z = prz(a)
-    try
-        z = maximum(filter(x -> abs(a(x)) < 1e-6, map(x -> x.re, z)))
-        return dual_check(s, push!(fn_acc, a), push!(err_acc, abs(eval(s, z))), z, width, depth - 1, depth_true, samples_domain)
-    catch
-        domain = range(xi_guess - 0.1 * abs(xi_guess), xi_guess + 0.1 * abs(xi_guess), 100)
-        plot(domain, map(x -> eval(s, x), domain))
-        savefig("preview.png")
-        println(z)
-        println(depth)
-        readline()
-        return dual_check(s, fn_acc, err_acc, 2 * xi_guess, width, depth, depth_true, samples_domain)
-    end
+    _, _, z = prz(approx)
+    z = maximum(map(x -> x.re, filter(x -> x.im < tol, z)))
+    update(s, 1e-2)
+    err = aaa(range(z - 0.1 * abs(z), z + 0.1 * abs(z), 5), x -> approx(x) - eval(s, x))
+    return dual_check(s, approx + err, push!(err_acc, abs(eval(s, z))), z, width, depth - 1)
 end
 
 
 # ERROR TESTING
-function error_statistics(err_acc, xi_init::Float64, samples::Int64, width::Int64, depth::Int64, reps::Int64)
-    if reps == 0
-        return foldl(.+, eachcol(err_acc)) / length(err_acc)
-    end
-
-    s_init = Surrogate(5)
+function error_of_h(xi_init::Float64, samples::Int64, width::Int64, depth::Int64)
+    s_init = Surrogate(100)
     domain = range(xi_init - 0.2 * abs(xi_init), xi_init + 0.2 * abs(xi_init), samples)
     a_s = aaa(domain,  xi -> eval(s_init, xi), mmax = Int64(floor(samples / 2)))
     _, _, z = prz(a_s)
-    z = maximum(filter(x -> abs(a_s(x)) < 1e-6, map(x -> x.re, z)))
-    _, _, err_new = dual_check(s_init, [a_s], [eval(s_init, z)], z, width, depth, depth, domain)
-    if isnothing(err_acc)
-        return error_statistics(err_new, xi_init, samples, width, depth, reps - 1)
-    end
-    return error_statistics(hcat(err_acc, err_new), xi_init, samples, width, depth, reps - 1)
+    z = maximum(map(x -> x.re, z))
+
+    _, err_new, _ = dual_check(s_init, a_s, [abs(eval(s_init, z))], z, width, depth)
+    return err_new
 end
 
 
 function err_analysis(xi_init::Float64, hyperparameters, depth::Int64, reps::Int64)
-    return map(h -> error_statistics(nothing, xi_init, h[1], h[2], depth, reps), hyperparameters)
+    e_of_h = () -> map(h -> error_of_h(xi_init, h[1], h[2], depth), hyperparameters)
+    errs = pmap(x -> e_of_h(), range(1, reps))
+    return foldl(.+, errs) ./ reps
 end
 
 
 samples_domain = 10:20
 width_domain = 4:10
-depth = 5
-reps = Int64(1e1)
+depth = 10
+reps = Int64(1e5)
 hyperparameters = Base.product(samples_domain, width_domain)
 errs = err_analysis(1.0, hyperparameters, depth, reps)
 serialize("hyperparameters.dat", hyperparameters)
@@ -97,7 +83,7 @@ n = length(hyperparameters)
 s = reshape(map(h -> h[1], hyperparameters), n)
 w = reshape(map(h -> h[2], hyperparameters), n)
 e = reshape(map(e -> foldl(+, e) / depth, errs), n)
-surface(s, w, e)
+scatter(s, w, e)
 xlabel!("samples")
 ylabel!("width")
 savefig("preview.png")
--- duality/test.jl
+++ duality/test.jl
@@ -1,52 +1,37 @@
-using LinearAlgebra, BaryRational, Serialization, Plots
+N = 2^20
+x = fill(1.0f0, N)  # a vector filled with 1.0 (Float32)
+y = fill(2.0f0, N)  # a vector filled with 2.0
 
+y .+= x             # increment each element of y with the corresponding element of x
 
-mutable struct Surrogate
-    a0::Float64
-    a1::Float64
-    s::Vector{ComplexF64}
-    P0::Matrix{ComplexF64}
-    P1::Matrix{ComplexF64}
-
-    function Surrogate(n::Int64)
-        a0 = rand(Float64)
-        a1 = rand(Float64)
-        s = rand(ComplexF64, n)
-        A = rand(ComplexF64, n, n);
-        P0 = (A + Adjoint(A)) / 2;
-        P1 = rand(ComplexF64, n, n);
-        new(a0, a1, s, P0, P1)
+function sequential_add!(y, x)
+    for i in eachindex(y, x)
+        @inbounds y[i] += x[i]
     end
+    return nothing
 end
 
 
-function eval(surr::Surrogate, xi::Float64)
-    M = (surr.a1 + xi) .* surr.P0 + surr.a0 .* surr.P1;
-    x = M \ surr.s;
-    return ((Adjoint(x) * surr.s).im - Adjoint(x) * surr.P0 * x).re    
+function parallel_add!(y, x)
+    Threads.@threads for i in eachindex(y, x)
+        @inbounds y[i] += x[i]
+    end
+    return nothing
 end
 
+using BenchmarkTools
+@btime sequential_add!($y, $x)
+@btime parallel_add!($y, $x)
 
-function update(surr::Surrogate, dx::Float64)
-    n = size(surr.s)[1]
-    A = dx * rand(ComplexF64, n, n);
-    surr.P0 += dx * rand(ComplexF64, n, n);
-    surr.P1 += (A + Adjoint(A)) / 2;
+using CUDA
+
+x_d = CUDA.fill(1.0f0, N)  # a vector stored on the GPU filled with 1.0 (Float32)
+y_d = CUDA.fill(2.0f0, N)  # a vector stored on the GPU filled with 2.0
+y_d .+= x_d
+
+function add_broadcast!(y, x)
+    CUDA.@sync y .+= x
+    return
 end
 
-
-a::BaryRational.AAAapprox + b::BaryRational.AAAapprox = BaryRational.AAAapprox(push(a.z, b.z), push(a.f, b.f), push(a.w, b.w), push(a.errvec, b.errvec))
-
-
-s = Surrogate(100)
-domain = range(0, 10, 20)
-approx = aaa(domain, x -> eval(s, x))
-println(typeof(approx))
-
-for i in range(1, 10)
-    approx = approx + err
-    plot(domain, [map(x -> eval(s, x), domain), map(approx, domain)])
-    savefig("preview.png")
-    readline()
-    update(s, 1e-3)
-end
+@btime add_broadcast!($y_d, $x_d)
